489569fa6ef760406f3ca1ab99f17fe8dd1d617a
sashka <sashka66rus@gmail.com>
2022-09-01 04:29:01 +0500

    Bug fixed

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+# 0.0.3 (2022-09-01)
+
+Bug fixed
+
+
+
 # 0.0.2 (2022-09-01)
 
 Removed a private object properties.<br/>
--- index.js
+++ index.js
@@ -1,23 +1,6 @@
 import { useSyncExternalStore } from 'react';
 
-class StateSub{
-	
-	constructor(store){
-		for(let state in store.initialState){
-			this.__newState(state, store.initialState[state]); // transferring initial states to store
-		}
-		delete store.initialState; // clearing initial states
-		Object.defineProperty(this, 'subTo', { // defining the subscription function
-			value: function(stateName){
-				if(stateName in this.__states){
-					return useSyncExternalStore(this.__subscribe.bind(this, stateName), () => this.__states[stateName]);
-				}
-				return undefined; // when someone tries to subscribe to an undefined state, the subscription will be skipped and returned "undefined"
-			}
-		});
-		this.__proto__ = store;
-	}
-
+const StateSubFunctions = {
 	__newState(stateName, value){ // defining accessors from initialState
 		this.__states[stateName] = value;
 		Object.defineProperty(this, stateName, {
@@ -29,7 +12,7 @@ class StateSub{
 				this.__broadcast(stateName); // broadcast to subscribers after any setting a new value to state. 
 			}
 		});
-	}
+	},
 
 	__subscribe(stateName, f){
 		if(!(stateName in this.__subsribers)){ // create a new room for subscribers, if it doesnt exist
@@ -37,17 +20,41 @@ class StateSub{
 		}
 		this.__subsribers[stateName].add(f); // adding a subscriber to a room
 		return ()=> this.__subsribers[stateName].delete(f); // return "subCleaner" for useSyncExternalStore (used when unmounting the component)
-	}
+	},
 
 	__broadcast(stateName){
 		if(stateName in this.__subsribers){
 			this.__subsribers[stateName].forEach(f => f());
 		}
+	},
+
+	__useSyncExternalStore(stateName){
+		return useSyncExternalStore(this.__subscribe.bind(this, stateName), () => this.__states[stateName]);
+	},
+
+	__states: {},
+
+	__subsribers: {},
+
+};
+
+class StateSub{
+	constructor(store){
+		StateSubFunctions.__proto__ = store;
+		this.__proto__ = StateSubFunctions;
+		for(let state in store.initialState){
+			this.__newState(state, store.initialState[state]); // transferring initial states to store
+		}
+		delete store.initialState; // clearing initial states
+		Object.defineProperty(this, 'subTo', { // defining the subscription function
+			value: function(stateName){
+				if(stateName in this.__states){
+					return this.__useSyncExternalStore(stateName);
+				}
+				return undefined; // when someone tries to subscribe to an undefined state, the subscription will be skipped and returned "undefined"
+			}
+		});
 	}
-
-	__states = {}
-
-	__subsribers = {}
 };
 
 export default StateSub;
